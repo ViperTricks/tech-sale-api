223f0cdf0939ac1e4fab873d2d1022088dc61712
Vipertricks <hodactoan181905@gmail.com>
2026-03-19 21:26:51 +0700

    debug

--- config/db.js
+++ config/db.js
@@ -3,9 +3,10 @@ require("dotenv").config();
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    port: process.env.DB_PORT
 });
 
 db.connect((err) => {
